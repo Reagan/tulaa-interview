8438056141f3cec119f5c31413469dd25897a1c1
Reagan Mbitiru <reagan@nectar.software>
2018-06-21 19:03:31 +0300

    added parenthesis checker

---
+++ src/tulaa/ParenthesisChecker.java
@@ -0,0 +1,36 @@
+package tulaa;
+
+import java.util.Vector;
+
+public class ParenthesisChecker {
+
+    public static void main(String[] args) {
+        ParenthesisChecker checker = new ParenthesisChecker();
+        System.out.println(checker.validate("()[]{}"));
+        System.out.println(checker.validate("([)]"));
+        System.out.println(checker.validate("()"));
+        System.out.println(checker.validate("(]"));
+    }
+
+    private boolean validateParenthesis(String str) {
+        if (null != str || str == "") {
+            Vector stack = new Vector();
+            for (char c : str.toCharArray()) {
+                if (c == '[' || c == '(' || c == '{')
+                    stack.add(c);
+
+                if (c == ']' && (stack.size() <= 0 || (char) stack.remove(stack.size() - 1) != '[')) return false;
+                if (c == '}' && (stack.size() <= 0 || (char) stack.remove(stack.size() - 1) != '{')) return false;
+                if (c == ')' && (stack.size() <= 0 || (char) stack.remove(stack.size() - 1) != '(')) return false;
+            }
+        }
+        return true;
+    }
+
+    private String validate(String parenthesis) {
+        if (validateParenthesis(parenthesis))
+            return String.format("%s is valid", parenthesis);
+        else
+            return String.format("%s is not valid", parenthesis);
+    }
+}
